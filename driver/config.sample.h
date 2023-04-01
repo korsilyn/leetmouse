55cf068efaecd2a6cbfb195422f1b636abf85cc0
Eugene Gladkov <jenya.love72@gmail.com>
2023-04-02 00:53:42 +0300

    Rewrite float -> fixedmath

--- driver/config.sample.h
+++ driver/config.sample.h
@@ -1,30 +1,24 @@
+#include "fixedptc.h"
 // Maximum number of packets allowed to be sent from the mouse at once. Linux's
-// default value is 8, which at least causes EOVERFLOW for my mouse (SteelSeries
-// Rival 600). Increase this, if 'dmesg -w' tells you to!
-#define BUFFER_SIZE 16
-
-/*
- * This should be your desired acceleration. It needs to end with an f.
- * For example, setting this to "0.1f" should be equal to
- * cl_mouseaccel 0.1 in Quake.
- */
+// default value is 8.
+// Increase this, if 'dmesg -w' tells you to!
+#define BUFFER_SIZE 8
 
 // Changes behaviour of the scroll-wheel. Default is 3.0f
-#define SCROLLS_PER_TICK 3.0f
+#define SCROLLS_PER_TICK fixedpt_rconst(3.0)
 
-// Emulate Windows' "Enhanced Pointer Precision" for my mouse (1000 Hz) by
-// approximating it with a linear accel
-#define SENSITIVITY 0.85f
-#define ACCELERATION 0.26f
-#define SENS_CAP 4.0f
-#define OFFSET 0.0f
-#define SPEED_CAP 0.0f
+// Main settings
+#define SENSITIVITY fixedpt_rconst(0.85)
+#define ACCELERATION fixedpt_rconst(0.26)
+#define SENS_CAP fixedpt_rconst(4.0)
+#define OFFSET fixedpt_rconst(0.0)
+#define SPEED_CAP fixedpt_rconst(0.0)
 
 // 1 is Linear, 2 is Classic, 3 is Motivity
 #define ACCELERATION_MODE 1
 
 // For exponential curves.
-#define EXPONENT 2.0f
+#define EXPONENT fixedpt_rconst(2.0)
 
 // Midpoint for sigmoid curves
-#define MIDPOINT 1.0f
+#define MIDPOINT fixedpt_rconst(1.0)
