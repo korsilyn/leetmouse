2163cd3bdb68d7b3a5ccede45337ceef499731be
Eugene Gladkov <jenya.love72@gmail.com>
2024-07-23 04:26:05 +0300

    Makefile for driver module (for NixOS usage)

--- driver/accel.h
+++ driver/accel.h
@@ -1,6 +1,9 @@
 #ifndef _ACCEL_H
 #define _ACCEL_H
 
+#include "fixedptc.h"
+
 void accelerate(int *x, int *y, int *wheel);
+void update_param(const char *str, fixedpt *result);
 
 #endif /* _ACCEL_H */
--- driver/config.sample.h
+++ driver/config.sample.h
@@ -1,16 +1,23 @@
 #include "fixedptc.h"
 // Maximum number of packets allowed to be sent from the mouse at once. Linux's
-// default value is 8.
-// Increase this, if 'dmesg -w' tells you to!
+// default value is 8, which at least causes EOVERFLOW for my mouse (SteelSeries
+// Rival 600). Increase this, if 'dmesg -w' tells you to!
 #define BUFFER_SIZE 8
+
+/*
+ * This should be your desired acceleration. It needs to end with an f.
+ * For example, setting this to "0.1f" should be equal to
+ * cl_mouseaccel 0.1 in Quake.
+ */
 
 // Changes behaviour of the scroll-wheel. Default is 3.0f
 #define SCROLLS_PER_TICK fixedpt_rconst(3.0)
 
-// Main settings
-#define SENSITIVITY fixedpt_rconst(0.85)
-#define ACCELERATION fixedpt_rconst(0.26)
-#define SENS_CAP fixedpt_rconst(4.0)
+// Emulate Windows' "Enhanced Pointer Precision" for my mouse (1000 Hz) by
+// approximating it with a linear accel
+#define SENSITIVITY fixedpt_rconst(0.3)
+#define ACCELERATION fixedpt_rconst(0.02)
+#define SENS_CAP fixedpt_rconst(2.0)
 #define OFFSET fixedpt_rconst(0.0)
 #define SPEED_CAP fixedpt_rconst(0.0)
 
